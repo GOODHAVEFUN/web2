ac9eddfc82e007c58df22f4a9ec951af6e02cd9a
abb60455 <abb60455@gmail.com>
2023-07-03 12:46:52 +0900

    no message

--- day02/Scripts/alias.sql
+++ day02/Scripts/alias.sql
@@ -1,32 +1,20 @@
--- 테이블의 컬럼이름을 바꿔서 조회
+-- 테이블의 컬럼 이름을 바꿔서 조회하기
 -- 테이블의 컬럼명을 별칭(별명)으로 설정하기
--- AS alias
-SELECT 
-first_name AS "이름" , 
-last_name AS "성", 
-SALARY AS "봉급", 
-EMPLOYEE_ID AS "사원번호"
-FROM EMPLOYEES;
+-- AS alias 
+SELECT 	employee_id AS "사원번호",
+	last_name AS "성", 
+	first_name AS "이름", 
+	salary AS "봉급"
+FROM employees;
 
--- AS 생략가능 
---단 띄어쓰기가 있을 경우 "" 생략 불가능
-SELECT 
-EMPLOYEE_ID 사원번호,
-first_name 이름 , 
-last_name 성, 
-SALARY 봉급
-FROM EMPLOYEES;
-
---[실습] 사원의 이름 , 성 , 봉급을 봉급기중 내림차순으로 정렬하여 조회하기 
---별명 사용하기
-SELECT FIRST_NAME 이름 , LAST_NAME 성, SALARY 봉급 
-FROM EMPLOYEES
-ORDER BY 봉급 DESC ;
+-- AS 생략 가능 "" 생략가능
+-- 단, 별명에 띄어쓰기가 있을때는 "" 생략 불가능
+SELECT employee_id "사원 번호",
+	last_name 성,
+	first_name 이름,
+	salary 봉급
+FROM employees;
 
 
---[실습2] 이름, 성을 조회하는데 봉급기준 오름 차순으로 정렬하여 조회하기
---별명 사용하기
 
-SELECT FIRST_NAME 이름 , LAST_NAME 성
-FROM EMPLOYEES
-ORDER BY SALARY ;
+
--- day02/Scripts/selectTest.sql
+++ day02/Scripts/selectTest.sql
@@ -11,15 +11,42 @@
 -- ; 하나의 명령이 끝나면 작성한다
 -- 세미콜론 이전에 줄바꿈을 해도 상관 없다
 
-SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES;
+-- SELECT FIRST_NAME, LAST_NAME FROM EMPLOYEES;
 -- 대문자로 작성해도 오류가 발생하지 않는다
 -- 여러개의 컬럼을 같이 보고 싶다면 , 콤마를 이용해서 사용할 수 있다
 -- HR계정으로 로그인되어있기 때문에 HR. 은 생략가능하다
 -- 계정명을 작성한 이유는 여러 계정에 같은 이름의 테이블 이름이 존재할 수 있기 때문이다
 -- 지금 로그인한 계정의 테이블 우선순위가 높기 때문에 일반적으로 계정명은 생략이 가능하다
 
---[실습]직원의 성 , 이름 , 핸드폰번호 , 이메일주소, 사원번호를 순서대로 한번에 조회
--- SELECT LAST_NAME ,FIRST_NAME, PHONE_NUMBER , EMAIL , EMPLOYEE_ID FROM hr.EMPLOYEES;
+-- [실습] 직원의 성, 이름, 핸드폰번호, 이메일주소, 사원번호를 한번에 조회하기(순서대로 조회되어야함)
+-- SELECT last_name, first_name, phone_number, email, employee_id FROM employees;
 
--- 모든 컬럼을  다 조회하고싶다면 *를  사용한다
-SELECT  * FROM EMPLOYEES;
+-- 모든 컬럼을 다 조회하고 싶다면 *를 사용한다
+SELECT * FROM EMPLOYEES;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
